59e31f0157b7c514d4feb50ccbfe8b547ab4b721
skulden <ewd.henrique@gmail.com>
2019-12-24 15:29:05 -0300

    Fim da análise

--- main/utilities/companies_symbols.R
+++ main/utilities/companies_symbols.R
@@ -4,19 +4,23 @@ url <- "https://br.advfn.com/bolsa-de-valores/bovespa/"
   
 companies_symbols = data.frame(empresa = character(), simbolo = character())
 
-try(
   
-    for(letter in c(LETTERS)){
-    
-    html <- read_html(glue(url,letter))
-    
-    page_table <- html %>% html_node("table") %>% html_table()
-    
+for(letter in c(LETTERS)){
+
+  html <- read_html(glue(url,letter))
+
+  page_table <- tryCatch(html %>% html_node("table") %>% html_table(), error = function(e) NULL)
+
+  if(!is.null(page_table)) {
+
     colnames(page_table) <- c("empresa", "simbolo")
     page_table$empresa <- toupper(page_table$empresa)
     companies_symbols <- rbind(companies_symbols, page_table)
-      
-    }
-)
+  
+  }
+  
+}
+
 
 companies_symbols$simbolo <- paste(companies_symbols$simbolo, ".SA", sep = "")
+
